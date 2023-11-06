24b255552f7e34915c47dfbd4e84ea70f2ab09ec
Michael Cotterell <mepcotterell@gmail.com>
2023-11-06 13:33:15 -0500

    initial commit

---
+++ R/tdcm-package.R
@@ -0,0 +1,11 @@
+#' @details
+#' See `vignette("tdcm", package = "tdcm")` for an overview of the **tdcm**
+#' package.
+#'
+#' @keywords internal
+#' @aliases tdcm-package NULL
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
