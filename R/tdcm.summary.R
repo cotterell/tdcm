565ae6c3fcdd41856d29c514e5eacb46b46cd2fa
Matthew James Madison <matthewm@uga.edu>
2024-11-06 14:44:22 -0500

    - added growth effects - citation and text edits changes

--- R/tdcm.summary.R
+++ R/tdcm.summary.R
@@ -4,27 +4,36 @@
 #'
 #' @details
 #' Provides a summary of TDCM results including item parameters, attribute posterior
-#' probabilities, transition posterior probabilities, classifications, growth,
+#' probabilities, transition posterior probabilities, classifications, growth, growth effects,
 #' transition probabilities, attribute correlations, several transition reliability metrics,
 #' and model fit. Includes longitudinal DCM reliability metrics developed by
-#' Schellman and Madison (2021).
+#' Schellman and Madison (2024).
 #'
 #'
 #' @param model a \code{gdina} object returned from the \code{\link{tdcm}} function.
 #'
-#' @param num.time.points the number of time points (i.e., measurement/testing occasions), integer \eqn{\ge 2}.
+#' @param transition.option option for reporting results. \code{= 1} compares the first time point to the last.
+#' \code{= 2} compares the first time point to every other time point.
+#' \code{= 3} compares successive time points. Default = 1.
 #'
-#' @param transition.option option for reporting results. \code{= 1} compares the first time point to the last. \code{= 2} compares the first time point to every other time point. \code{= 3} compares successive time points. Default = 1.
-#'
-#' @param classthreshold probability threshold for establishing proficiency from examinee posterior probabilities. Default is .50, which maximizes overall classification accuracy. It can be set to a lower value to minimize false negatives (i.e., misclassifying proficient examinees as non-proficient) or set to a higher value to minimize false positives (i.e., misclassifying non-proficient examinees as proficient).
+#' @param classthreshold probability threshold for establishing proficiency from examinee
+#' posterior probabilities. Default is .50, which maximizes overall classification accuracy.
+#' It can be set to a lower value to minimize false negatives (i.e., misclassifying proficient
+#' examinees as non-proficient) or set to a higher value to minimize false positives (i.e.,
+#' misclassifying non-proficient examinees as proficient).
 #'
 #' @param attribute.names optional vector of attribute names to include in results output.
 #'
 #' @return A list with the following items:
 #' \itemize{
-#'    \item \code{$item.parameters}: LCDM item parameter estimates from the specified DCM.
+#'    \item \code{$item.parameters}: item parameter estimates from the specified DCM.
 #'
 #'    \item \code{$growth}: proficiency proportions for each time point and each attribute
+#'
+#'    \item \code{$growth.effects}: growth effect size estimates for each attribute and specified transitions
+#'    including growth in proficiency proportion, odds ratio = odds proficiency at later time point divided by odds
+#'    of proficiency at earlier time point, and Cohen's h (arcsine-transformed difference in proportions;
+#'    Cohen, 1988)
 #'
 #'    \item \code{$transition.probabilities}: conditional attribute proficiency transition probability matrices
 #'
@@ -34,32 +43,53 @@
 #'
 #'    \item \code{$most.likely.transitions}: examinee most likely transitions for each attribute and transition
 #'
-#'    \item \code{$classifications}: examinee classifications determined by the specified threshold applied to the posterior probabilities
+#'    \item \code{$classifications}: examinee classifications determined by the specified threshold applied
+#'    to the posterior probabilities
 #'
-#'    \item \code{$reliability}: estimated transition reliability metrics for each attribute for the specified transitions. “pt bis” = longitudinal point biserial metric; “info gain” = longitudinal information gain metric; “polychor” = longitudinal tetrachoric metric; “ave max tr” = average maximum transition posterior metric; “P(t>k)” = proportion of examinee marginal attribute transition posteriors greater than k; “wt pt bis” = weighted longitudinal point biserial; “wt info gain” = weighted longitudinal information gain.
+#'    \item \code{$reliability}: estimated transition reliability metrics for each attribute for the specified
+#'    transitions. “pt bis” = longitudinal point biserial metric; “info gain” = longitudinal information gain
+#'    metric; “polychor” = longitudinal tetrachoric metric; “ave max tr” = average maximum transition posterior
+#'    metric; “P(t>k)” = proportion of examinee marginal attribute transition posteriors greater than k;
+#'    “wt pt bis” = weighted longitudinal point biserial; “wt info gain” = weighted longitudinal information gain.
 #'
 #'    \item \code{$att.corr}: estimated attribute correlation matrix
 #'
-#'    \item \code{$model.fit}: Several model fit indices and tests are output including item root mean square error of approximation (RMSEA; von Davier, 2005), mean RMSEA, bivariate item fit statistics (Chen et al., 2013), and absolute fit statistics such as mean absolute deviation for observed and expected item correlations (MADcor; DiBello, Roussons, & Stout, 2007), and standardized root mean square root of squared residuals (SRMSR; Maydeu-Olivares, 2013)
+#'    \item \code{$model.fit}: Several model fit indices and tests are output including item root mean square
+#'    error of approximation (RMSEA; von Davier, 2005), mean RMSEA, bivariate item fit statistics
+#'    (Chen et al., 2013), and absolute fit statistics such as mean absolute deviation for observed and
+#'    expected item correlations (MADcor; DiBello, Roussos, & Stout, 2007), and standardized root mean
+#'    square root of squared residuals (SRMSR; Maydeu-Olivares, 2013)
 #'
 #' }
 #'
 #'
-#' @references Chen, J., de la Torre, J. ,& Zhang, Z. (2013). Relative and absolute fit evaluation in cognitive diagnosis modeling. \emph{Journal of Educational Measurement, 50}, 123-140.
+#' @references Chen, J., de la Torre, J. ,& Zhang, Z. (2013). Relative and absolute fit evaluation in cognitive
+#' diagnosis modeling. \emph{Journal of Educational Measurement, 50}, 123-140.
 #'
-#' DiBello, L. V., Roussos, L. A., & Stout, W. F. (2007). \emph{Review of cognitively diagnostic assessment and a summary of psychometric models}. In C. R. Rao and S. Sinharay (Eds.), Handbook of Statistics, Vol. 26 (pp.979–1030). Amsterdam: Elsevier.
+#' Cohen, J. (1988). \emph{Statistical Power Analysis for the Behavioral Sciences} (2nd ed.). Hillsdale, NJ:
+#' Lawrence Erlbaum Associates, Publishers.
 #'
-#' Johnson, M. S., & Sinharay, S. (2020). The reliability of the posterior probability of skill attainment in diagnostic classification models. \emph{Journal of Educational Measurement, 47}(1), 5 – 31.
+#' DiBello, L. V., Roussos, L. A., & Stout, W. F. (2007). \emph{Review of cognitively diagnostic assessment and
+#' a summary of psychometric models}. In C. R. Rao and S. Sinharay (Eds.), Handbook of Statistics, Vol. 26
+#' (pp.979–1030). Amsterdam: Elsevier.
 #'
-#' Madison, M. J. (2019). Reliably assessing growth with longitudinal diagnostic classification models. \emph{Educational Measurement: Issues and Practice, 38}(2), 68-78.
+#' Johnson, M. S., & Sinharay, S. (2020). The reliability of the posterior probability of skill attainment
+#' in diagnostic classification models. \emph{Journal of Educational Measurement, 47}(1), 5 – 31.
 #'
-#' Maydeu-Olivares, A. (2013). Goodness-of-fit assessment of item response theory models (with discussion). \emph{Measurement: Interdisciplinary Research and Perspectives, 11}, 71-137.
+#' Madison, M. J. (2019). Reliably assessing growth with longitudinal diagnostic classification models.
+#' \emph{Educational Measurement: Issues and Practice, 38}(2), 68-78.
 #'
-#' Schellman, M., & Madison, M. J. (2021, July). \emph{Estimating the reliability of skill transition in longitudinal DCMs}. Paper presented at the 2021 International Meeting of the Psychometric Society.
+#' Maydeu-Olivares, A. (2013). Goodness-of-fit assessment of item response theory models (with discussion).
+#' \emph{Measurement: Interdisciplinary Research and Perspectives, 11}, 71-137.
 #'
-#' Templin, J., & Bradshaw, L. (2013). Measuring the reliability of diagnostic classification model examinee estimates. \emph{Journal of Classification, 30}, 251-275.
+#' Schellman, M., & Madison, M. J. (2024). Estimating the reliability of skill transition in longitudinal DCMs.
+#'  \emph{Journal of Educational and Behavioral Statistics}.
 #'
-#' von Davier M. (2008). A general diagnostic model applied to language testing data. \emph{The British journal of mathematical and statistical psychology, 61}(2), 287–307.
+#' Templin, J., & Bradshaw, L. (2013). Measuring the reliability of diagnostic classification model examinee
+#' estimates. \emph{Journal of Classification, 30}, 251-275.
+#'
+#' von Davier M. (2008). A general diagnostic model applied to language testing data. \emph{The British journal
+#' of mathematical and statistical psychology, 61}(2), 287–307.
 
 #'
 #' @export
@@ -72,20 +102,22 @@
 #' qmat1 <- data.tdcm01$q.matrix
 #'
 #' # estimate TDCM with invariance assumed and full LCDM
-#' m1 <- TDCM::tdcm(dat1, qmat1, num.time.points = 2, invariance = TRUE, rule = "GDINA")
+#' m1 <- TDCM::tdcm(dat1, qmat1, num.time.points = 2, invariance = TRUE, rule = "LCDM")
 #'
 #' # summarize results with tdcm.summary function
-#' results1 <- TDCM::tdcm.summary(m1, num.time.points = 2)
+#' results1 <- TDCM::tdcm.summary(m1, transition.option = 1)
 #' results1$item.parameters
 #' results1$growth
+#' results1$growth.effects
 #' results1$transition.probabilities
 #' results1$reliability
 #' head(results1$most.likely.transitions)
 #' results1$model.fit$Item.RMSEA
 #' }
-tdcm.summary <- function(model, num.time.points, transition.option = 1, classthreshold = .50,
+tdcm.summary <- function(model, transition.option = 1, classthreshold = .50,
                          attribute.names = c()) {
 
+  num.time.points = model$numtimepoints
   num.items <- length(model$itemfit.rmsea) # total items
   items <- num.items / num.time.points # Items per time point
   num.atts <- ncol(model$attribute.patt.splitted) / num.time.points # Number of attribute measured
@@ -93,7 +125,8 @@ tdcm.summary <- function(model, num.time.points, transition.option = 1, classthr
   # posterior probabilities
   postprobs <- matrix(NA, nrow = model$N, ncol = num.atts * num.time.points)
   postprobs <- round(model$pattern[, 6:(6 + num.atts * num.time.points - 1)], 3)
-  colnames(postprobs) <- t(outer(c(paste("T", 1:num.time.points, sep = "")), c(paste("A", 1:num.atts, sep = "")), FUN = "paste0"))
+  colnames(postprobs) <- t(outer(c(paste("T", 1:num.time.points, sep = "")), c(paste("A", 1:num.atts, sep = "")),
+                                 FUN = "paste0"))
 
   # estimated classifications
   estclass <- data.frame((postprobs > classthreshold) * 1)
@@ -111,24 +144,36 @@ tdcm.summary <- function(model, num.time.points, transition.option = 1, classthr
 
   ### Growth Block ###
   if (transition.option == 1) {
-    o1 <- summary.option1(model = model, num.atts = num.atts, num.time.points = num.time.points, attribute.names = attribute.names)
+    o1 <- summary.option1(model = model, num.atts = num.atts, num.time.points = num.time.points,
+                          attribute.names = attribute.names)
     trans <- o1$trans
     growth <- o1$growth
     rel <- o1$rel
+    growth.effects <- o1$growth.effects
+
   } else if (transition.option == 2) {
-    o2 <- summary.option2(model = model, num.atts = num.atts, num.time.points = num.time.points, attribute.names = attribute.names)
+    o2 <- summary.option2(model = model, num.atts = num.atts, num.time.points = num.time.points,
+                          attribute.names = attribute.names)
     trans <- o2$trans
     growth <- o2$growth
     rel <- o2$rel
+    growth.effects <- o2$growth.effects
+
+
   } else {
-    o3 <- summary.option3(model = model, num.atts = num.atts, num.time.points = num.time.points, attribute.names = attribute.names)
+    o3 <- summary.option3(model = model, num.atts = num.atts, num.time.points = num.time.points,
+                          attribute.names = attribute.names)
     trans <- o3$trans
     growth <- o3$growth
     rel <- o3$rel
+    growth.effects <- o3$growth.effects
+
   }
 
+
   ### Parameters Block ###
-  param <- summary.param(model = model, num.atts = num.atts, num.items = num.items, num.time.points = num.time.points)
+  param <- summary.param(model = model, num.atts = num.atts, num.items = num.items, num.time.points =
+                           num.time.points)
 
   # attribute correlations
   cor <- model$polychor
@@ -157,8 +202,11 @@ tdcm.summary <- function(model, num.time.points, transition.option = 1, classthr
 
   newList <- list(
     "item.parameters" = param, "growth" = growth, "transition.probabilities" = trans,
+    "growth.effects" = growth.effects,
     "posterior.probabilities" = postprobs, "classifications" = estclass, "reliability" = rel$metrics,
     "transition.posteriors" = rel$transposts, "most.likely.transitions" = rel$mostlikelytransitions,
     "option" = transition.option, "att.corr" = cor, "model.fit" = mf, "numgroups" = 1)
+
   return(newList)
+
 }
